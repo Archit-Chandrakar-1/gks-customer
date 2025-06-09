9c10dee6e2af389ce82407e464215ec853326680
Archit Chandrakar <archit1chandrakar@gmail.com>
2025-06-09 10:06:58 +0530

    commit

--- components/houseServices/electronicService/electronicService.jsx
+++ components/houseServices/electronicService/electronicService.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header1 from '@/components/headers/Header1'; // Import your header component
 import Footer1 from '@/components/footer/Footer1'; // Import your footer component
-import ServicePage from '@/components/servicePage/ServicePage'; // Import the ServicePage component
+
 import ServicePage from '@/components/servicePage/ServicePage';
 import { electronicData } from '@/data/electronicData';
 import './electronicService.css';
